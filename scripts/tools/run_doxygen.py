67618ec4ec336d2dad36ece0030058efd0fd4c33
Stefano Lusardi <lusardi.stefano@gmail.com>
2024-08-06 18:35:08 +0200

    Update run_doxygen.py

--- scripts/tools/run_doxygen.py
+++ scripts/tools/run_doxygen.py
@@ -22,7 +22,7 @@ import sys
 
 def parse():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--doxyfile_path", help="Path of the Doxyfile to be used", default="video_io/docs/Doxyfile")
+    parser.add_argument("--doxyfile_path", help="Path of the Doxyfile to be used", default="inference_client/docs/Doxyfile")
     return parser.parse_args()
 
 def main():
