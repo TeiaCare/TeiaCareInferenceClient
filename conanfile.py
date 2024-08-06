5c9c805b05c721a55f9d7dba64ffb2a6fac149e3
Stefano Lusardi <stefano.lusardi@teiacare.com>
2024-08-06 17:23:20 +0200

    Replaced conanfile.txt with conanfile.py to address Linux-only settings.

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
+# Copyright 2024 TeiaCare
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from conans import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain
+import re
 
 def get_project_version():
-    with open('VERSION') as version_file:
-        # TODO: validate Regex format
-        return version_file.read().strip()
+    with open('VERSION', encoding='utf8') as version_file:
+        version_regex = r'^\d+\.\d+\.\d+$'
+        version = version_file.read().strip()
+        if re.match(version_regex, version):
+            return version
+        else:
+            raise ValueError(f"Invalid version detected into file VERSION: {version}")
 
 class TeiaCareInferenceClient(ConanFile):
     name = "teiacare_inference_client"
@@ -29,6 +48,20 @@ class TeiaCareInferenceClient(ConanFile):
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
+
+        self.options["grpc"].codegen=True
+        self.options["grpc"].csharp_ext=False
+        self.options["grpc"].cpp_plugin=True
+        self.options["grpc"].csharp_plugin=False
+        self.options["grpc"].node_plugin=False
+        self.options["grpc"].objective_c_plugin=False
+        self.options["grpc"].php_plugin=False
+        self.options["grpc"].python_plugin=False
+        self.options["grpc"].ruby_plugin=False
+        self.options["grpc"].secure=False
+
+        if self.settings.os == "Linux":
+            self.options["grpc"].with_libsystemd=False
 
     def generate(self):
         tc = CMakeToolchain(self)
