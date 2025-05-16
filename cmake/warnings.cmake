80dfab6bceaa40a38f63274752cf20cbe05e2490
Stefano Lusardi <stefano.lusardi@teiacare.com>
2025-05-16 09:04:15 +0200

    Updated cache key. Silence warnings on Visual Studio 2019.

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -12,6 +12,8 @@ function(add_warnings_as_errors TARGET)
     if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
         target_compile_options(${TARGET} PRIVATE -Werror)
     elseif(MSVC)
+        # On Visual Studio disable warnings 4100 and 4127 from 3rd party dependencies.
+        target_compile_options(${TARGET} PRIVATE /wd4100 /wd4127)
         target_compile_options(${TARGET} PRIVATE /WX)
     endif()
 endfunction()
